29d0eaa1af38b02c4f70d837f5206839e49624a8
stefanaz2 <sfrunza@seneca.ca>
2022-10-13 18:50:14 -0400

    added a few colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors:{
+      'Rausch':'#FF5A5F',
+      'Babu':'#00A699',
+      'Arches':'#FC642D',
+      'Hof':'#484848',
+      'Foggy':'#767676'
+    },
+    extend: {
+      fontFamily:{
+        montserrat: ['Montserrat'],
+      },
+    },
   },
   plugins: [],
 };
